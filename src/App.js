2e25c910bb1ab1a23991c22fb846a5f9908ed389
Kalkazar <joecurrystodder@gmail.com>
2023-06-13 12:01:45 -0400

    added react-star-ratings, got it working, and removed unnecessary packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import StarRatings from 'react-star-ratings';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0,
+    };
+    this.changeRating = this.changeRating.bind(this);
+  }
+  changeRating(newRating) {
+    this.setState({
+      rating: newRating
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <StarRatings
+            rating={this.state.rating}
+            starRatedColor="blue"
+            changeRating={this.changeRating}
+            numberOfStars={6}
+            name='rating'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
