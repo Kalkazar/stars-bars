cb9073634aa50f066d809d9ef887fb6aa711f998
Kalkazar <joecurrystodder@gmail.com>
2023-06-13 14:28:48 -0400

    added new voting system, and a placeholder way of displaying that voting system

--- src/Demo.js
+++ src/Demo.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import StarRatings from 'react-star-ratings';
+import star from './star.svg';
 import './Demo.css';
 
 class Demo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rating: 0,
+            ratings: [0, 0, 0, 0, 0, 0], // TODO: consider defining some other way
         };
-        this.changeRating = this.changeRating.bind(this);
+        this.updateRatings = this.updateRatings.bind(this);
+        this.resetRatings = this.resetRatings.bind(this);
+        this.displayRatingsTally = this.displayRatingsTally.bind(this);
     }
-    changeRating(newRating) {
-        this.setState({
-            rating: newRating
-        });
+    updateRatings(newVote) {
+        const ratings = this.state.ratings;
+        ratings[newVote - 1]++;
+        this.setState({ ratings });
+    }
+    resetRatings() {
+        const ratings = Array(this.state.ratings.length).fill(0);
+        this.setState({ ratings });
+    }
+    displayRatingsTally() { // TODO: consider making this its own component
+        // also consider just defining this in the "render" method below
+        return this.state.ratings.map(
+            (ratingTally, index) => <div className='tally-row'>
+                <div style={{ textAlign: 'left' }}>
+                    {index === 0 ? "Zero Stars" : [...Array(index)].map(
+                        (e, i) => <img src={star} className='scorecount-star' key={i} />
+                    )}
+                </div>
+                <div style={{ textAlign: 'right' }}>
+                    {ratingTally}
+                </div>
+            </div>
+        ); // TODO: CLEAN THIS UP
     }
     render() {
-        return <div className="Demo">
+        return <div className='Demo'>
             <StarRatings
-                rating={this.state.rating}
-                starRatedColor="blue"
-                changeRating={this.changeRating}
-                numberOfStars={6}
+                changeRating={this.updateRatings}
+                numberOfStars={this.state.ratings.length}
                 name='rating'
             />
+            <div>
+                {this.displayRatingsTally()}
+            </div>
+            <button onClick={this.resetRatings}>
+                Reset Values
+            </button>
         </div>;
     }
 }
